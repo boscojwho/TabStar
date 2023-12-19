06946303f1945a75b9bf530ea50029562ad9fcc1
Bosco Ho <boscojwho@gmail.com>
2023-12-19 02:55:09 -0800

    ~ Set package platform versions. ^ Fix package dependencies.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "TabStar",
+    platforms: [.iOS(.v16), .macCatalyst(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TabStar",
             targets: ["TabStar"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TabStar"),
+            name: "TabStar",
+            dependencies: [.product(name: "Dependencies", package: "swift-dependencies")]
+        ),
         .testTarget(
             name: "TabStarTests",
             dependencies: ["TabStar"]),
